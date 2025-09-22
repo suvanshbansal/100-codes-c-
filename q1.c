f7d673b5d2219793c95dd81bd198220c61fb8111
Suvansh Bansal <suvanshbansal2006@gmail.com>
2025-09-22 15:20:21 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+int b;
+printf("enter a number");
+scanf("%d", &a);
+printf("enter a number");
+scanf("%d", &b);
+printf("sum of two numbers entered %d", a+b);
+return 0;
+}
